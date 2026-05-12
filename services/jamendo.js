4853414dfce112f2bfcb8ac3c9768a75e02ee611
sithushane <sithushane55@gmail.com>
2026-05-12 16:10:10 +0630

    Add files via upload

---
+++ services/jamendo.js
@@ -0,0 +1,5 @@
+export async function fetchMusic(mood) {
+    console.log(`Searching music for mood: ${mood}`);
+    // လောလောဆယ် အစမ်း music url တစ်ခုပဲ ပြန်ပေးထားပါမယ်
+    return "https://www.bensound.com/bensound-music/bensound-ukulele.mp3";
+}
---
+++ services/pexels.js
@@ -0,0 +1,5 @@
+export async function fetchStockVideo(keyword) {
+    console.log(`Searching video for: ${keyword}`);
+    // လောလောဆယ် အစမ်းအနေနဲ့ sample video url တစ်ခုပဲ ပြန်ပေးထားပါမယ်
+    return "https://www.w3schools.com/html/mov_bbb.mp4"; 
+}
